6e3c153ffa68cd5ac5111b441831000891e703c0
firasglai <glaifiras44@gmail.com>
2025-04-30 02:18:23 +0100

    perf: :arrow_up: Upgrading Nextjs 13 -> 15

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -8,8 +8,8 @@ import ContactFormEmail from "@/email/contact-form-email";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-  const senderEmail = formData.get("senderEmail");
-  const message = formData.get("message");
+  const senderEmail = formData.get("senderEmail") as string;
+  const message = formData.get("message") as string;
 
   // simple server-side validation
   if (!validateString(senderEmail, 500)) {
